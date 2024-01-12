6d425fd8b55fdc87f9d3c0d7e34ca73357608bc2
Miguel Moreira <miguel.m.moreira@tecnico.ulisboa.pt>
2024-01-12 01:52:45 +0000

    Correção esquecimento de aspas

--- Estatística/proj1.py
+++ Estatística/proj1.py
@@ -1,17 +1,17 @@
 def read_txt_file(file_path):
     try:
         with open(file_path, 'r') as file:
-            data = file.read().replace('\n', '')  
-            return data.strip()  
+            data = file.read().replace('\n', '')
+            return data.strip()
     except FileNotFoundError:
         return "File not found."
 
 def convert_to_string(data):
 
-    lines = data.split('\n')  
-    binary_string = ''.join(lines)  
+    lines = data.split('\n')
+    binary_string = ''.join(lines)
     return binary_string
 
-teste = read_txt_file("seq1.txt)
+teste = read_txt_file("seq1.txt")
 
 # Obrigado ChatGPT
