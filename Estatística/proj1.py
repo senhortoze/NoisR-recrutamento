1a581c6cd4b1d1e287cd0648dc79a584f82b9a54
Miguel Moreira <miguel.m.moreira@tecnico.ulisboa.pt>
2024-01-14 21:43:01 +0000

    Projeto de estatística - entrega

--- Estatística/proj1.py
+++ Estatística/proj1.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def read_txt_file(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -9,9 +11,28 @@ def read_txt_file(file_path):
 def convert_to_string(data):
 
     lines = data.split('\n')
+    print(lines)
     binary_string = ''.join(lines)
     return binary_string
 
 teste = read_txt_file("seq1.txt")
 
+numeros = [] #{número: frequência}
+#for i in range(0,64):
+ #   numeros[i] = 0
+
+str_check = ""
+
+for bit in teste:
+    str_check += bit
+
+    if len(str_check) == 6:
+        numero_atual = 0
+        for i in range(0,6):
+            numero_atual += 2**i * int(str_check[-(i+1)])
+        numeros.append(numero_atual)
+        str_check = ""
+
+plt.hist(numeros, 64, rwidth=0.9)
+plt.show()
 # Obrigado ChatGPT
